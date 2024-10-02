52e3397a90bb7c4e64b8e0bde69b85fb267a0ead
Drag0s98 <dragostanase1698@gmail.com>
2024-10-02 12:22:42 +0200

    feat: added conventional commit and basic packages

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import App from "./App";
 
 import "./index.css";
 import "./styles/main.scss";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <ToastContainer />
-  </StrictMode>
+  </StrictMode>,
 );
